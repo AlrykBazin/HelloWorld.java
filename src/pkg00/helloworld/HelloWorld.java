07c2e571021b4a8e429fe790ba968041f002baa7
abazin5 <abazin5@350028R-F115X11.lise.kreizenn.bzh>
2026-09-25 11:15:27 +0200

    EXO 4

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
+import java.util.Scanner;
 
 /**
  *
@@ -13,9 +14,23 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-          System.out.println("Hello World by MB on GH");
-
+    /*public static void main(String[] args) {
+          System.out.println("Hello World by MB on GH");*/
+/*Exo 4*/
+     Scanner sc = new Scanner(System.in);
+        int X = 0;
+        int Y = 0;
+        int tmp = 0;
+        
+  System.out.print("Saisir X :");
+  X = sc.nextInt();
+  System.out.print("Saisir Y :");
+  Y = sc.nextInt();
+  
+  tmp = X;
+  X=Y;
+  Y=tmp;
+  System.out.println(" X : "+ X +" Y :"+ Y +"");
     }
     
 }
